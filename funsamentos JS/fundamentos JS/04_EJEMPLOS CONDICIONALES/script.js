3e051b2cc267da1ef7e41ef525bc2dd21762fb44
kevito0 <kevinvargas@liceovvh.cl>
2024-04-04 14:00:52 -0300

    OK

---
+++ funsamentos JS/fundamentos JS/04_EJEMPLOS CONDICIONALES/script.js
@@ -0,0 +1,12 @@
+function identificarMayorEdad()
+{
+    //Identificar si es mayor o menor edad
+    let edad = prompt("Por favor ingrese su Edad: ");
+
+    if(edad >= 18 ){
+        alert(`Es mayor de edad- tiene: ${edad}`);
+    } else {
+        alert(`Es menor de Edad - tiene: ${edad}`)
+    }
+
+}         
